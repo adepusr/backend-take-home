f1b3ceac49466a89822e8c4a56f0c72254fa7e43
adepusr <adepu.sravankumar94@gmail.com>
2019-05-03 10:30:47 -0400

    added store file to store inmemory variables

---
+++ store/index.js
@@ -0,0 +1,14 @@
+/*
+Acts as Inmemory Store for this INVENTORY application 
+*/
+
+// Stores list of all add products
+var PRODUCTS = [];
+
+// Stores list of all added warehouses
+var WAREHOUSES = [];
+
+module.exports = {
+    PRODUCTS,
+    WAREHOUSES
+};
